701eea771e03d914360c7474bb8ba0bc3e1da3f5
RUCHIT302 <gevariyaruchit@gmail.com>
2024-07-29 18:59:39 +0530

    Update Navbar.js
    change the id of name bar
    
    Signed-off-by: RUCHIT302 <gevariyaruchit@gmail.com>

--- src/componet/Navbar.js
+++ src/componet/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar(props) {
   <div className="container-fluid">
     
     <a className="navbar-brand" href="#">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
